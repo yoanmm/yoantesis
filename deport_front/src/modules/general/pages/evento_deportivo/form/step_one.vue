440eadbb0d07a94cc68c9430fa1ae36695729594
Andy Rivero Martin <riveroandy02@gmail.com>
2025-06-12 13:50:34 -0400

    feat(fe):design wizard event

---
+++ deport_front/src/modules/general/pages/evento_deportivo/form/step_one.vue
@@ -0,0 +1,72 @@
+<template>
+  <tc-form
+    :feedbacks="mb.statics('Evento_deportivo').feedbacks"
+    :vobject="$v"
+    nested="evento_deportivo"
+    ref="form"
+    class="form-row"
+  >
+    <tc-form-item class="form-group mb-0 col-md-6 px-3">
+      <label>Nombre del evento</label>
+      <tc-input
+        placeholder="Ingrese el nombre"
+        name="nombre_evento"
+        v-model="evento_deportivo.nombre_evento"
+      ></tc-input>
+    </tc-form-item>
+
+    <tc-form-item class="form-group mb-0 col-md-6 px-3">
+      <label>Descripción</label>
+      <tc-input
+        placeholder="Ingrese la descripción"
+        name="descripcion_evento"
+        v-model="evento_deportivo.descripcion_evento"
+      ></tc-input>
+    </tc-form-item>
+
+    <tc-form-item class="form-group mb-0 col-md-6 px-3">
+      <label>Fecha inicio</label>
+      <tc-date-picker name="fecha_inicio" v-model="evento_deportivo.fecha_inicio"></tc-date-picker>
+    </tc-form-item>
+
+    <tc-form-item class="form-group mb-0 col-md-6 px-3">
+      <label>Fecha fin</label>
+      <tc-date-picker name="fecha_fin" v-model="evento_deportivo.fecha_fin"></tc-date-picker>
+    </tc-form-item>
+
+    <tc-form-item class="form-group mb-0 col-md-6 px-3">
+      <label>Curso</label>
+      <tc-input placeholder="Ej: 2020-2021" name="curso" v-model="evento_deportivo.curso"></tc-input>
+    </tc-form-item>
+
+    <tc-form-item class="form-group mb-0 col-md-6 px-3">
+      <label>Regla</label>
+      <tc-autocomplete
+        placeholder="Seleccione la regla"
+        name="id_regla_evento"
+        ref="select_regla_evento"
+        idKey="id_regla_evento"
+        textKey="nombre_regla"
+        :successFeed="false"
+        :defaultValue="evento_deportivo.id_regla_evento"
+        v-model="evento_deportivo.id_regla_evento"
+        :url="mb.statics('Evento_deportivo_regla').select_2_url"
+      />
+    </tc-form-item>
+  </tc-form>
+</template>
+
+<script>
+import * as mb from "@/helpers/loaders/model.load";
+
+export default {
+  name: "StepOne",
+  validations: mb.statics("Evento_deportivo").validations,
+  data() {
+    return {
+      mb,
+      evento_deportivo: mb.instance("Evento_deportivo"),
+    };
+  },
+};
+</script>
---
+++ deport_front/src/modules/general/pages/evento_deportivo/form/step_three.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <persona_atleta_table
+      :columns="columns"
+      table_name="Persona_atleta"
+      id_table="id_atleta"
+      ref="persona_atleta_table"
+      :params_search="params_search"
+      :paginate="paginate"
+    />
+  </div>
+</template>
+
+<script>
+import * as mb from "@/helpers/loaders/model.load";
+import persona_atleta_table from "@/modules/general/pages/persona_atleta/list/persona_atleta.table";
+
+export default {
+  name: "StepThree",
+  components: {
+    persona_atleta_table,
+  },
+  data() {
+    return {
+      columns: mb.statics("Persona_atleta").columns,
+      params_search: { relations: ["persona", "tipo"] },
+      paginate: false,
+      mb,
+    };
+  },
+  methods: {
+    loadTableData() {
+      this.$refs.persona_atleta_table.load_data();
+    },
+  },
+  mounted() {
+    this.loadTableData();
+  },
+};
+</script>
