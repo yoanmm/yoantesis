4854b3631debe84574e43d79d40415ee35537ed3
yoanmm <yoanmm2001@gmail.com>
2026-02-23 15:30:46 -0500

    Fix typo in class name and enhance persona import functionality with JSON file support

--- deport_front/src/modules/general/pages/persona/list/persona_list.vue
+++ deport_front/src/modules/general/pages/persona/list/persona_list.vue
@@ -14,7 +14,7 @@
         </a-breadcrumb-item>
       </a-breadcrumb>
     </div>
-    <div class="rwo col-md-6">
+    <div class="row col-md-6">
       <h3>Personas</h3>
     </div>
     <div class="container">
@@ -28,40 +28,38 @@
               <a-tooltip placement="topLeft" title="Eliminar elementos seleccionados">
                 <a-button icon="delete" type="danger" @click="showDeleteConfirm">Eliminar</a-button>
               </a-tooltip>
+              <a-tooltip placement="topLeft" title="Importar desde JSON">
+                <a-button icon="upload" type="dashed" @click="triggerFileInput">Importar JSON</a-button>
+                <input ref="fileInput" type="file" accept="application/json" style="display:none" @change="handleFileChange" />
+              </a-tooltip>
             </a-button-group>
           </div>
         </div>
-      </div>
-    </div>
-    <div>
-      <a-modal
-        @cancel="onCloseModal"
-        :title="selected_persona.get_id() ? 'Actualizar persona' : 'Añadir persona'"
-        class="modal-form"
-        width="55rem"
-        :visible="show_modal_form"
-        :destroyOnClose="true"
-        on-ok="handleOk"
-        :header="null"
-        :footer="null"
-        :maskClosable="false"
-      >
-        <persona_form :modal="true" :model="selected_persona" />
-      </a-modal>
-      <div style="margin-left: 15px">
-        <persona_table
-          :columns="columns"
-          table_name="Persona"
-          id_table="id_persona"
-          ref="persona_table"
-          :params_search="params_search"
-          :paginate="paginate"
-        />
+        <a-modal
+          v-model="show_modal_form"
+          :header="null"
+          :footer="null"
+          :maskClosable="false"
+        >
+          <persona_form :modal="true" :model="selected_persona" />
+        </a-modal>
+        <div style="margin-left: 15px">
+          <persona_table
+            :columns="columns"
+            table_name="Persona"
+            id_table="id_persona"
+            ref="persona_table"
+            :params_search="params_search"
+            :paginate="paginate"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
+          import Persona from '@/modules/general/entities/persona/persona.model';
 import * as utils from "@/helpers/helpers/utils";
 import * as mb from "@/helpers/loaders/model.load";
 import persona_form from "../form/persona_form";
@@ -73,9 +71,9 @@ export default {
     return {
       close_modal: this.onCloseModal,
       show_form: this.show_form,
-      load_data: () => {
-        this.$refs.persona_table.load_data();
-      },
+       load_data: () => {
+         this.$refs.persona_table.load_data();
+       },
       setSelectedPersona: this.setSelectedPersona,
     };
   },
@@ -96,6 +94,36 @@ export default {
     persona_table,
   },
   methods: {
+              triggerFileInput() {
+                this.$refs.fileInput.click();
+              },
+              async handleFileChange(event) {
+                const file = event.target.files[0];
+                if (!file) return;
+                try {
+                  const text = await file.text();
+                  const personas = JSON.parse(text);
+                  if (!Array.isArray(personas)) throw new Error('El archivo no contiene un array');
+                  let exitos = 0, fallos = 0;
+                  for (const p of personas) {
+                    try {
+                      const persona = new Persona(p);
+                      await persona.save();
+                      exitos++;
+                    } catch (e) {
+                      fallos++;
+                    }
+                  }
+                  utils.openNotificationWithIcon(
+                    fallos === 0 ? 'success' : 'warning',
+                    'Importación finalizada',
+                    `Éxitos: ${exitos}, Fallos: ${fallos}`
+                  );
+                  this.$refs.persona_table.load_data();
+                } catch (error) {
+                  utils.openNotificationWithIcon('error', 'Error al importar', error.message);
+                }
+              },
     setSelectedPersona(model) {
       this.selected_persona = model;
     },
